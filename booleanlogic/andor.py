aa073bdc0b896fa7b986eaf2c91827afff74914b
santiagos <sguillenz935@gmail.com>
2023-07-23 18:07:24 +0200

    booleans

---
+++ booleanlogic/andor.py
@@ -0,0 +1,18 @@
+a = 33
+b = 33
+
+if a == 33 and b == 33:
+    print(a + b)
+
+if a == 33 or b == 33:
+    print(a * b)
+
+#EXAMPLE BIEN
+
+object_size = 10 
+proximity = 9000
+
+if object_size > 5 and proximity < 9000 :
+    print("Evasive maneuvers required ")
+else:
+    print("Object poses no threat.")
